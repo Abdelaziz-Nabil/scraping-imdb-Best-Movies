8027a6382cd48f846401fbfc9fe910f899be9f14
Abdelaziz Nabil <71048405+Abdelaziz-Nabil@users.noreply.github.com>
2023-04-19 05:24:08 +0200

    imdb.py

--- imdbv1/spiders/imdb.py
+++ imdbv1/spiders/imdb.py
@@ -5,7 +5,7 @@ class ImdbSpider(scrapy.Spider):
     name = "imdb"
     allowed_domains = ["www.imdb.com"]
 
-    User_agent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+    User_agent = "$YOUR USER AGENT HERE"
 
     def start_requests(self):
         yield scrapy.Request(
